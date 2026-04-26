57a2a90f49f8c77295b7e90f1a86c5ca20b53d96
Luiz Mantovani <luizclaudioamantovani@gmail.com>
2026-04-26 00:40:34 -0300

    Estilizado e organizado pagina details

--- src/details/index.jsx
+++ src/details/index.jsx
@@ -1,9 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function Details() {
   const { name } = useParams();
   const [pokemon, setPokemon] = useState(null);
+  const [generation, setGeneration] = useState("");
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+      .then(res => res.json())
+      .then(data => setGeneration(data.generation.name));
+  }, [name]);
+
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -14,17 +22,19 @@ function Details() {
   if (!pokemon) return <p>Carregando...</p>;
 
   return (
-    <div>
+    <div className="details-container">
       <h1>{pokemon.name}</h1>
       <img src={pokemon.sprites.front_default} />
 
+      <p>ID: {pokemon.id}</p>
+      <p>Geração: {generation.replace("generation-", "Gen ").toUpperCase()}</p>
+      <p>Habilidades: {pokemon.abilities.map(a => a.ability.name).join(", ")}</p>
       <p>Altura: {pokemon.height}</p>
       <p>Peso: {pokemon.weight}</p>
+      <p>Tipos: {pokemon.types.map(t => t.type.name).join(", ")}</p>
 
-      <p>
-        Tipos:
-        {pokemon.types.map(t => t.type.name + " ")}
-      </p>
+      <button className="btnestilo"><Link className="links" to="/list">Voltar</Link></button>
+
     </div>
   );
 }
