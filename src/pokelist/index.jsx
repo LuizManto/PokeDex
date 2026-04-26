49b133fb26212340dda98cf7fa2a7bac13a5b554
Luiz Mantovani <luizclaudioamantovani@gmail.com>
2026-04-25 23:17:40 -0300

    Ajustado tamanho do site

--- src/pokelist/index.jsx
+++ src/pokelist/index.jsx
@@ -3,24 +3,29 @@ import { Link } from "react-router-dom";
 
 function Pokelist() {
   const [pokemons, setPokemons] = useState([]);
+  const [limit, setLimit] = useState(20);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
       .then(res => res.json())
       .then(data => setPokemons(data.results));
-  }, []);
+  }, [limit]);
 
   return (
     <div>
-      <h1>Pokédex</h1>
-
-      {pokemons.map(p => (
-        <div key={p.name}>
-          <Link to={`/pokemon/${p.name}`}>
-            {p.name}
-          </Link>
-        </div>
-      ))}
+      <div className="pokelist">
+        <h1>Pokédex</h1>
+        {pokemons.map(p => (
+          <div key={p.name}>
+            <Link to={`/pokemon/${p.name}`}>
+              {p.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <button className="btnestilo" onClick={() => setLimit(limit + 20)}>
+        Carregar mais
+      </button>
     </div>
   );
 }
