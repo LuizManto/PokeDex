a4edc30802c98dd31ca13a3137ec37776d3fb5e9
Luiz Mantovani <luizclaudioamantovani@gmail.com>
2026-04-26 22:07:40 -0300

    finalizado

--- src/search/index.jsx
+++ src/search/index.jsx
@@ -3,19 +3,39 @@ import { useState } from "react";
 function Search() {
   const [name, setName] = useState("");
   const [pokemon, setPokemon] = useState(null);
+  const [erro, setErro] = useState("");
 
   const buscar = () => {
+    setErro("");
+    setPokemon(null);
+
+    if (!name.trim()) {
+      setErro("Digite um nome!");
+      return;
+    }
+
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      .then(res => res.json())
-      .then(data => setPokemon(data));
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Pokemon não encontrado!");
+        }
+        return res.json();
+    })
+    .then(data => setPokemon(data))
+    .catch(() => {
+      setErro("Pokemon não encontrado, digitou corretamente?")
+    });
   };
 
   return (
-    <div>
+    <div className="search-container">
       <h1>Buscar Pokémon</h1>
 
       <input className="busca" onChange={(e) => setName(e.target.value)} />
       <button className="btnbusca" onClick={buscar}>Buscar</button>
+
+      {erro && <p>{erro}</p>}
 
       {pokemon && (
         <div>
